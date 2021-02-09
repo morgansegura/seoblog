f79643e24e757517cec5869186bda20085ab5b77
Morgan Segura <morgansegura@gmail.com>
2021-02-09 11:41:37 -0800

    Sets up mongodb and mongoose ad the first api route

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+// import routes
+const blogRoute = require('./routes/blog')
+
+// app
+const app = express()
+
+// db
+mongoose
+	.connect(process.env.DATABASE_CLOUD, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('DB connected'))
+
+// middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+// cors
+if (process.env.NODE_ENV === 'development') {
+	app.use(cors({ origin: `${process.env.CLIENT_URL}` }))
+}
+
+// routes middleware
+app.use('/api', blogRoute)
+
+// port
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
+})
