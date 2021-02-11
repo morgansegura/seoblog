9479eb242e383f1d227f57292b91d66312051979
Morgan Segura <morgansegura@gmail.com>
2021-02-10 23:15:02 -0800

    Adds user and admin dashboard templates

---
+++ frontend/pages/admin/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const AdminIndex = () => {
+	return (
+		<Layout>
+			<div className='w-full max-w-6xl mx-auto p-8'>
+				<h2>Admin Page</h2>
+			</div>
+		</Layout>
+	)
+}
+
+export default AdminIndex
---
+++ frontend/pages/user/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const UserIndex = () => {
+	return (
+		<Layout>
+			<div className='w-full max-w-6xl mx-auto p-8'>
+				<h2>User Dashboard</h2>
+			</div>
+		</Layout>
+	)
+}
+
+export default UserIndex
