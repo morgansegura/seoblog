fb92e87f6895351b2f694f13b4be6ac5f31a5866
Morgan Segura <morgansegura@gmail.com>
2021-02-12 00:06:10 -0800

    Cretaes a helper function for hdnling errors

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,5 +1,6 @@
 const Category = require("../models/category")
 const slugify = require("slugify")
+const dbErrorHandler = require("../helpers/dbErrorHandler")
 
 exports.create = (req, res) => {
 	const { name } = req.body
@@ -10,7 +11,7 @@ exports.create = (req, res) => {
 	category.save((err, data) => {
 		if (err) {
 			return res.status(404).json({
-				error: err,
+				error: dbErrorHandler(err),
 			})
 		}
 		res.json(data)
