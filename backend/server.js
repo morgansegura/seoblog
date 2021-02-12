ff24e44be624a600c90c1ed56bb7b81fb1283289
Morgan Segura <morgansegura@gmail.com>
2021-02-12 00:02:20 -0800

    Adds category schema and routes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require("express")
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const mongoose = require("mongoose")
+require("dotenv").config()
 
 // import routes
-const blogRoutes = require('./routes/blog')
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
+const blogRoutes = require("./routes/blog")
+const authRoutes = require("./routes/auth")
+const userRoutes = require("./routes/user")
+const categoryRoutes = require("./routes/category")
 
 // app
 const app = express()
@@ -22,22 +23,23 @@ mongoose
 		useFindAndModify: false,
 		useUnifiedTopology: true,
 	})
-	.then(() => console.log('DB connected'))
+	.then(() => console.log("DB connected"))
 
 // middleware
-app.use(morgan('dev'))
+app.use(morgan("dev"))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
 // cors
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
 	app.use(cors({ origin: `${process.env.CLIENT_URL}` }))
 }
 
 // routes middleware
-app.use('/api', blogRoutes)
-app.use('/api', authRoutes)
-app.use('/api', userRoutes)
+app.use("/api", blogRoutes)
+app.use("/api", authRoutes)
+app.use("/api", userRoutes)
+app.use("/api", categoryRoutes)
 
 // port
 const port = process.env.PORT || 8000
