e7ec51d88a086b74ad599c37b1bf29443b9832d6
Morgan Segura <morgan@CyanAlbacore.attlocal.net>
2021-02-11 20:34:29 -0800

    Adds user avatar

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,49 +1,106 @@
-import React from 'react'
-import Router from 'next/router'
-import Link from 'next/link'
-import { isAuth, signout } from '../actions/auth'
-import { APP_NAME } from '../config'
+import React, { Fragment, useState } from "react"
+import Router from "next/router"
+import Link from "next/link"
+import { isAuth, signout } from "../actions/auth"
+import { APP_NAME } from "../config"
 
-const Header = ({ children, className }) => {
+const Header = ({ className }) => {
+	const [toggleProfileNav, setToggleProfileNav] = useState(false)
+
+	const handleProfileNav = () => {
+		setToggleProfileNav(!toggleProfileNav)
+		console.log(toggleProfileNav)
+	}
 	return (
 		<header className={className}>
-			<div className='w-full max-w-6xl mx-auto py-4 px-8 flex justify-between items-center'>
+			<div className='flex items-center justify-between w-full max-w-6xl px-8 py-4 mx-auto'>
 				<Link href='/'>
 					<a>{APP_NAME}</a>
 				</Link>
-				<div className='flex-1 flex justify-between items-center text-sm text-gray-500'>
-					<div className='flex-1 flex justify-center space-x-4'>
+				<div className='flex items-center justify-between flex-1 text-sm text-gray-500'>
+					<div className='flex justify-center flex-1 space-x-4'>
 						<Link href='/'>
-							<a className='hover:text-gray-700 transition duration-300 ease-out'>
+							<a className='transition duration-300 ease-out hover:text-gray-700'>
 								Home
 							</a>
 						</Link>
-						<div>Link2</div>
 					</div>
-					<div className='flex justify-end items-center space-x-4'>
+					<div className='flex items-center justify-end space-x-4'>
 						{!isAuth() ? (
-							<React.Fragment>
+							<Fragment>
 								<Link href='/signin'>
-									<a className='hover:text-gray-700 transition duration-300 ease-out'>
+									<a className='transition duration-300 ease-out hover:text-gray-700'>
 										Signin
 									</a>
 								</Link>
 								<Link href='/signup'>
-									<a className='cursor-pointer rounded py-1 px-2 bg-blue-500 text-blue-100 hover:bg-blue-700 transition duration-300 ease-out'>
+									<a className='px-2 py-1 text-blue-100 transition duration-300 ease-out bg-blue-500 rounded cursor-pointer hover:bg-blue-700'>
 										Signup
 									</a>
 								</Link>
-							</React.Fragment>
+							</Fragment>
 						) : (
-							<React.Fragment>
-								<div
-									onClick={() =>
-										signout(() => Router.replace(`/signin`))
-									}
-									className='cursor-pointer rounded py-1 px-2 bg-blue-500 text-blue-100 hover:bg-blue-700 transition duration-300 ease-out'>
-									Signout
+							<Fragment>
+								<div className='relative flex-shrink-0 ml-5'>
+									<div>
+										<button
+											type='button'
+											className='flex bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500'
+											id='user-menu'
+											aria-haspopup='true'>
+											<span className='sr-only'>
+												Open user menu
+											</span>
+											<img
+												className='w-8 h-8 rounded-full'
+												src='https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80'
+												alt=''
+											/>
+										</button>
+									</div>
+
+									<div
+										onClick={handleProfileNav}
+										className={`absolute right-0 z-10 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 ${
+											toggleProfileNav
+												? "transform opacity-100 scale-100"
+												: "transform opacity-0 scale-95"
+										} transition-transform duration-300 ease-linear`}
+										role='menu'
+										aria-orientation='vertical'
+										aria-labelledby='user-menu'>
+										{isAuth() && isAuth().role === 1 ? (
+											<Link href='/admin'>
+												<a
+													className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
+													role='menuitem'>
+													Your Dashboard
+												</a>
+											</Link>
+										) : (
+											<Link href='/user'>
+												<a
+													className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
+													role='menuitem'>
+													Your Dashboard
+												</a>
+											</Link>
+										)}
+										<div
+											onClick={() =>
+												signout(() =>
+													Router.replace(`/signin`)
+												)
+											}>
+											<a
+												className='block px-4 py-2 text-sm text-gray-700 cursor-pointer hover:bg-gray-100'
+												role='menuitem'>
+												Sign out
+											</a>
+										</div>
+									</div>
 								</div>
-							</React.Fragment>
+							</Fragment>
 						)}
 					</div>
 				</div>
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,13 +1,14 @@
-import { loadGetInitialProps } from 'next/dist/next-server/lib/utils'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { loadGetInitialProps } from "next/dist/next-server/lib/utils"
+import Document, { Html, Head, Main, NextScript } from "next/document"
 
 class SEOBlogDocument extends Document {
 	render() {
 		return (
 			<Html lang='en'>
 				<Head>
+					{/*<
 					<meta charSet='UTF-8' />
-					{/*<meta
+					meta
 						name='viewport'
 						content='width=device-width, initial-scale=1.0'
 					/> */}
