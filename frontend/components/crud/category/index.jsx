b1a5d3d3ea50dfdae75ade2670de46145aa7af74
Morgan Segura <morgansegura@gmail.com>
2021-02-13 02:13:15 -0800

    CRUD categories

--- frontend/components/crud/category/index.jsx
+++ frontend/components/crud/category/index.jsx
@@ -8,9 +8,16 @@ import {
 	getCategories,
 	removeCategory,
 } from "../../../actions/category"
-import { BiCheckCircle, BiError } from "react-icons/bi"
+import {
+	BiCheckCircle,
+	BiError,
+	BiMinusCircle,
+	BiToggleLeft,
+	BiToggleRight,
+} from "react-icons/bi"
 
 const Category = () => {
+	const [showTag, setShowTag] = useState(false)
 	const [values, setValues] = useState({
 		name: "",
 		error: false,
@@ -20,12 +27,12 @@ const Category = () => {
 		reload: false,
 	})
 
-	const { name, error, success, categories, removed } = values
+	const { name, error, success, categories, removed, reload } = values
 	const token = getCookie("token")
 
 	useEffect(() => {
 		loadCategories()
-	}, [])
+	}, [reload])
 
 	const loadCategories = () => {
 		getCategories().then(data => {
@@ -40,12 +47,47 @@ const Category = () => {
 	const showCategories = () => {
 		return categories.map((c, i) => {
 			return (
-				<button
+				<div
+					onClick={() => deleteConfirm(c.slug)}
 					key={i}
-					className='px-3 py-1 text-sm border border-gray-200 rounded'>
+					className='flex items-center px-3 py-1 mt-2 mr-2 text-sm transition duration-300 border border-gray-200 rounded cursor-pointer group hover:shadow'>
 					{c.name}
-				</button>
+					<span
+						title={`Delete ${c.name}`}
+						className={`inline-flex items-center ml-2 px-0.5 py-0.5 rounded-full text-xs font-medium bg-transition text-gray-900 group-hover:bg-red-100 transition duration-300 ease-out  ${
+							showTag ? "inline-flex" : "hidden"
+						}`}>
+						<BiMinusCircle className='w-4 h-4' />
+					</span>
+				</div>
 			)
+		})
+	}
+
+	const deleteConfirm = slug => {
+		let answer = window.confirm(
+			"Are you sure you want to delete this category?"
+		)
+		if (answer) {
+			deleteCategory(slug)
+		}
+		// deleteCategory(slug)
+	}
+
+	const deleteCategory = slug => {
+		removeCategory(slug, token).then(data => {
+			if (data.error) {
+				console.log(data.error)
+			} else {
+				setValues({
+					...values,
+					error: false,
+					success: false,
+					name: "",
+					removed: !removed,
+					reload: !reload,
+				})
+			}
 		})
 	}
 
@@ -55,7 +97,14 @@ const Category = () => {
 			if (data.error) {
 				setValues({ ...values, error: data.error, success: false })
 			} else {
-				setValues({ ...values, error: false, success: true, name: "" })
+				setValues({
+					...values,
+					error: false,
+					success: false,
+					name: "",
+					removed: false,
+					reload: !reload,
+				})
 			}
 		})
 	}
@@ -67,7 +116,6 @@ const Category = () => {
 			success: false,
 			removed: "",
 		})
-		console.log("handleChange")
 	}
 
 	const showSuccess = () => {
@@ -86,7 +134,7 @@ const Category = () => {
 			return (
 				<Toast
 					className='my-4 text-red-700 bg-red-50'
-					message='Category already exists!'
+					message={error}
 					icon={<BiError className='w-5 h-5 text-red-700' />}
 				/>
 			)
@@ -102,6 +150,10 @@ const Category = () => {
 				/>
 			)
 		}
+	}
+
+	const mouseMoveHandler = e => {
+		setValues({ ...values, error: false, sucess: false, removed: "" })
 	}
 
 	const newCategoryForm = () => (
@@ -122,7 +174,7 @@ const Category = () => {
 						placeholder='Type the category name.'
 					/>
 				</div>
-				<div class='pt-4 sm:flex-shrink-0 sm:flex sm:items-center'>
+				<div className='pt-4 sm:flex-shrink-0 sm:flex sm:items-center'>
 					<Button type='submit'>Create Category</Button>
 				</div>
 			</div>
@@ -133,21 +185,79 @@ const Category = () => {
 		<Fragment>
 			{showSuccess()}
 			{showError()}
-			{showRemoved()}
 
-			{newCategoryForm()}
-
-			{values.categories.length && (
-				<div className='relative p-6 mt-6 border border-gray-200 rounded'>
-					<h3 className='mb-3 text-xs font-semibold text-gray-600 uppercase'>
-						Category List
-						<span class='inline-flex items-center ml-2 px-1.5 py-0.5 rounded-full text-xs font-medium bg-indigo-100 text-indigo-800'>
-							{values.categories.length}
-						</span>
-					</h3>
-					<div className='flex space-x-2'>{showCategories()}</div>
-				</div>
-			)}
+			<div onMouseMove={mouseMoveHandler}>
+				{newCategoryForm()}
+				{showRemoved()}
+				{values.categories.length && (
+					<div className='relative p-6 mt-6 border border-gray-200 rounded'>
+						<div className='flex justify-between mb-3'>
+							<h3 className='text-xs font-semibold text-gray-600 uppercase'>
+								Category List
+								<span className='inline-flex items-center ml-2 px-1.5 py-0.5 rounded-full text-xs font-medium bg-indigo-100 text-indigo-800'>
+									{values.categories.length}
+								</span>
+							</h3>
+							<div className=''>
+								<button
+									onClick={() => setShowTag(!showTag)}
+									type='button'
+									className={`relative inline-flex flex-shrink-0 h-6 transition-colors duration-200 ease-in-out bg-gray-200 border-2 border-transparent rounded-full cursor-pointer w-11 focus:outline-none ${
+										showTag
+											? "bg-indigo-600"
+											: "bg-gray-200"
+									}`}
+									aria-pressed='false'>
+									<span className='sr-only'>Use setting</span>
+									<span
+										className={`relative inline-block w-5 h-5 transition duration-200 ease-in-out transform translate-x-0 bg-white rounded-full shadow pointer-events-none ring-0 ${
+											showTag
+												? "translate-x-5"
+												: "translate-x-0"
+										}`}>
+										<span
+											className={`absolute inset-0 flex items-center justify-center w-full h-full transition-opacity duration-200 ease-in opacity-100 ${
+												showTag
+													? "opacity-0 ease-out duration-100"
+													: "opacity-100 ease-in duration-200"
+											}`}
+											aria-hidden='true'>
+											<svg
+												className='w-3 h-3 text-gray-400 bg-white'
+												fill='none'
+												viewBox='0 0 12 12'>
+												<path
+													d='M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2'
+													stroke='currentColor'
+													strokeWidth='2'
+													strokeLinecap='round'
+													strokeLinejoin='round'
+												/>
+											</svg>
+										</span>
+										{/*<!-- Enabled: "opacity-100 ease-in duration-200", Not Enabled: "opacity-0 ease-out duration-100" -->*/}
+										<span
+											className={`absolute inset-0 flex items-center justify-center w-full h-full transition-opacity duration-100 ease-out opacity-0 ${
+												showTag
+													? "opacity-100 ease-in duration-200"
+													: "opacity-0 ease-out duration-100"
+											}`}
+											aria-hidden='true'>
+											<svg
+												className='w-3 h-3 text-indigo-600 bg-white'
+												fill='currentColor'
+												viewBox='0 0 12 12'>
+												<path d='M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z' />
+											</svg>
+										</span>
+									</span>
+								</button>
+							</div>
+						</div>
+						<div className='flex flex-wrap'>{showCategories()}</div>
+					</div>
+				)}
+			</div>
 		</Fragment>
 	)
 }
