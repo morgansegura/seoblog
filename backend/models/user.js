3ccdea032aae2a3ba04d2b871c0c8580ffb2dab7
Morgan Segura <morgan@jointhebibleproject.com>
2021-06-11 14:54:04 -0700

    Sets up validators for user auth

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,51 +1,56 @@
 const mongoose = require('mongoose')
 const crypto = require('crypto')
 
-const userSchema = new mongoose.Schema({
-	userName: {
-		type: String,
-		trim: true,
-		required: true,
-		maxLength: 32,
-		unique: true,
-		index: true,
-		lowerCase: true,
+const userSchema = new mongoose.Schema(
+	{
+		userName: {
+			type: String,
+			trim: true,
+			required: true,
+			max: 32,
+			unique: true,
+			index: true,
+			lowercase: true,
+		},
+		userName: {
+			type: String,
+			trim: true,
+			required: true,
+			max: 32,
+		},
+		email: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true,
+			lowercase: true,
+		},
+		profile: {
+			type: String,
+			required: true,
+		},
+		hashedPassword: {
+			type: String,
+			required: true,
+		},
+		salt: String,
+		about: {
+			type: String,
+		},
+		role: {
+			type: Number,
+			trim: true,
+		},
+		photo: {
+			data: Buffer,
+			contentType: String,
+		},
+		resetPasswordLink: {
+			data: String,
+			default: '',
+		},
 	},
-	userName: {
-		type: String,
-		trim: true,
-		required: true,
-		maxLength: 32,
-	},
-	email: {
-		type: String,
-		trim: true,
-		required: true,
-		unique: true,
-		lowerCase: true,
-	},
-	profile: {
-		type: String,
-		required: true,
-	},
-	hashedPassword: {
-		type: String,
-		required: true,
-	},
-	salt: String,
-	about: {
-		type: String,
-	},
-	role: {
-		type: Number,
-		trim: true,
-	},
-	photo: {
-		data: Buffer,
-		contentType: String,
-	},
-	resetPasswordLink: {
-		data: String,
-		default: '',
-	},
-})
+	{ timestamp: true }
+)
+
+module.exports = mongoose.model('User', userSchema)
